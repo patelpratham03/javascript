4327eb0d81d221ede6e5448540cba0aef9f8ba9e
Pratham Patel <prathampatel0301@gmail.com>
2025-08-29 19:21:59 +0530

    save

---
+++ 05_loops/while_do_loops.js
@@ -0,0 +1,19 @@
+{let index=0;
+while (index<=10) {
+    console.log(`index is ${index}`);
+    index++;    
+}}
+
+let ar1=["one","two","three","four"]
+let ar=0;
+while (ar < ar1.length) {
+    console.log(`ar1 is ${ar1[ar]}`);
+    ar+=1;
+
+    
+}
+let index=0;
+do {
+    console.log(`index is ${index}`);
+    index++;
+} while (index<=10);
